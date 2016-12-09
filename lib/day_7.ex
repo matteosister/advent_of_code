7b94266a991308f1f18d5fa9de48e550c3a4455a
Matteo Giachino <matteog@gmail.com>
2016-12-09 10:01:23 +0100

    day 7

--- lib/day_7.ex
+++ lib/day_7.ex
@@ -1,13 +1,25 @@
 defmodule Day7 do
   @doc """
-  iex> Day7.run_on_input(File.read!("inputs/day_7"))
+  iex> Day7.input_supports_tls(File.read!("inputs/day_7"))
   118
   """
-  def run_on_input(input) do
+  def input_supports_tls(input) do
     input
     |> String.split("\n")
     |> Enum.reject(&(&1 == ""))
     |> Enum.filter(&supports_tls?/1)
+    |> Enum.count
+  end
+
+  @doc """
+  iex> Day7.input_supports_ssl(File.read!("inputs/day_7"))
+  260
+  """
+  def input_supports_ssl(input) do
+    input
+    |> String.split("\n")
+    |> Enum.reject(&(&1 == ""))
+    |> Enum.filter(&supports_ssl?/1)
     |> Enum.count
   end
 
@@ -38,6 +50,40 @@ defmodule Day7 do
       inside |> find_groups_of(4) |> Enum.any?(&contains_abba?/1)
     end)
     outsides_has_abba && not insides_has_abba
+  end
+
+  @doc """
+  iex> Day7.supports_ssl? "aba[bab]xyz"
+  true
+
+  iex> Day7.supports_ssl? "xyx[xyx]xyx"
+  false
+
+  iex> Day7.supports_ssl? "aaa[kek]eke"
+  true
+
+  iex> Day7.supports_ssl? "zazbz[bzb]cdb"
+  true
+  """
+  def supports_ssl?(ip) do
+    outsides = String.split(ip, ~r/\[[a-z]+\]/)
+    insides = ~r/\[([a-z]+)\]/
+    |> Regex.scan(ip)
+    |> Enum.map(fn [_, v] -> v end)
+
+    outside_aba = outsides
+    |> Enum.flat_map(&find_groups_of(&1, 3))
+    |> Enum.map(&get_aba/1)
+    |> Enum.reject(&is_nil/1)
+    |> Enum.map(fn [a, b, _] -> [b, a, b] end)
+
+    inside_aba = insides
+    |> Enum.flat_map(&find_groups_of(&1, 3))
+    |> Enum.map(&get_aba/1)
+    |> Enum.reject(&is_nil/1)
+
+    outside_aba
+    |> Enum.any?(&(&1 in inside_aba))
   end
 
   @doc """
@@ -81,4 +127,24 @@ defmodule Day7 do
   def contains_abba?(<< a :: size(8), a :: size(8), a :: size(8), a :: size(8) >>), do: false
   def contains_abba?(<< a :: size(8), b :: size(8), b :: size(8), a :: size(8) >>), do: true
   def contains_abba?(_), do: false
+
+  @doc """
+  iex> Day7.get_aba("aba")
+  'aba'
+
+  iex> Day7.get_aba("ctc")
+  'ctc'
+
+  iex> Day7.get_aba("cts")
+  nil
+
+  iex> Day7.get_aba("cta")
+  nil
+
+  iex> Day7.get_aba("aaa")
+  nil
+  """
+  def get_aba(<< a :: size(8), a :: size(8), a :: size(8) >>), do: nil
+  def get_aba(<< a :: size(8), b :: size(8), a :: size(8) >>), do: [a, b, a]
+  def get_aba(_), do: nil
 end
